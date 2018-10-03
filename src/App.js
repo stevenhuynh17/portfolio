621f91ec6457b64ab9982461bc816bf0d2812e2a
Steven <stevenhuynh17@gmail.com>
2018-10-02 17:09:40 -0700

    header template done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 
+import Header from './components/Header'
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>TESTING</h1>
+        <Header />
       </div>
     );
   }
