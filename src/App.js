315a2dbf0c2b01dfa8a25525c5526a61ca75a697
Steven <stevenhuynh17@gmail.com>
2018-10-04 19:42:45 -0700

    css able to work

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 
-import Header from './components/Header'
+import Navigation from './components/Navigation'
+
+import './css/grayscale.css'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Header />
+      <div id="page-top">
+        <Navigation />
       </div>
     );
   }
---
+++ src/components/Navigation.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+import { Button, Navbar, Nav, NavItem } from 'react-bootstrap'
+
+class Navigation extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar fixedTop className="navbar navbar-expand-lg navbar-light" id="mainNav">
+          <div className="container">
+            <a className="navbar-brand js-scroll-trigger" href="#page-top">Start Bootstrap</a>
+            <Button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+              Menu
+              <i className="fas fa-bars"></i>
+            </Button>
+            
+            <Nav className="collapse navbar-collapse" id="navbarResponsive">
+              <NavItem eventKey={1} href="#">
+                About
+              </NavItem>
+              <NavItem eventKey={2} href="#">
+                Projects
+              </NavItem>
+              <NavItem eventKey={3} href="#">
+                Contact
+              </NavItem>
+            </Nav>
+          </div>
+        </Navbar>
+      </div>
+    )
+  }
+}
+
+export default Navigation;
