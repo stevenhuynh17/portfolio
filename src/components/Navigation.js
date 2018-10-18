3684d343490b30ef7a5edc34b2abad2ac3a64f17
Steven <stevenhuynh17@gmail.com>
2018-10-18 13:41:38 -0700

    header looks good to go

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -6,25 +6,25 @@ class Navigation extends Component {
   render() {
     return (
       <div>
-        <Navbar fixedTop className="navbar navbar-expand-lg navbar-light" id="mainNav">
-          <div className="container">
-            <a className="navbar-brand js-scroll-trigger" href="#page-top">Start Bootstrap</a>
-            <Button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-              Menu
-              <i className="fas fa-bars"></i>
-            </Button>
-            <Nav className="collapse navbar-collapse" id="navbarResponsive">
-              <NavItem eventKey={1} href="#">
+        <Navbar fixedTop className="" id="mainNav">
+          <Navbar.Header>
+            <Navbar.Brand>
+              <a className="navbar-brand" href="#page-top">Start Bootstrap</a>
+            </Navbar.Brand>
+          </Navbar.Header>
+          <Navbar.Collapse>
+            <Nav pullRight className="collapse navbar-collapse" id="navbarResponsive">
+              <NavItem className="nav-item" eventKey={1} href="#">
                 About
               </NavItem>
-              <NavItem eventKey={2} href="#">
+              <NavItem className="nav-item" eventKey={2} href="#">
                 Projects
               </NavItem>
-              <NavItem eventKey={3} href="#">
+              <NavItem className="nav-item" eventKey={3} href="#">
                 Contact
               </NavItem>
             </Nav>
-          </div>
+          </Navbar.Collapse>
         </Navbar>
       </div>
     )
