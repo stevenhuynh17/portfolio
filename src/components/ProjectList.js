f202e5ad3f1d619d611e7149451a01d2d09c4fc8
Steven <stevenhuynh17@gmail.com>
2018-10-29 14:49:46 -0700

    updated bootstrap. will have to refactor

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react'
-import demo from '../img/ipad.png'
+import { Image } from 'react-bootstrap'
+import demo from '../img/demo-image-01.jpg'
 
 class ProjectList extends Component {
   render() {
     return(
       <div className="container">
-        <h4>TESTING</h4>
-        <img src={demo} alt=""/>
-        <img src={"../img/ipad.png"}/>
-        <img src="../img/ipad.png"/>
+        <div className="row justify-content-center no-gutters mb-5 mb-lg-0">
+          <div className="col-lg-6">
+            <Image src={demo} alt="" responsive/>
+          </div>
+          <div className="col-lg-6">
+            <h4>TESTING</h4>
+          </div>
+        </div>
+
+
+        <div className="col-lg-6">
+          <h4>TESTING</h4>
+        </div>
+        <div className="col-lg-6">
+          <Image src={demo} alt="" responsive/>
+        </div>
       </div>
     )
   }
