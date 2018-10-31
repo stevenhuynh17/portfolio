cd7f92322e7d49e4b02c3f761afa90191481747c
Steven <stevenhuynh17@gmail.com>
2018-10-30 17:18:39 -0700

    got the top bar to hide when scrolling at a certain range

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,25 +1,50 @@
 import React, { Component } from 'react';
 
 class Navigation extends Component {
+  state = {
+    hide: null
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.navbarCollapse);
+    this.setState({
+      hide: false
+    })
+  }
+
+  navbarCollapse = (event) => {
+
+    if(window.pageYOffset > 100) {
+      this.setState({
+        hide: true
+      })
+    } else {
+      this.setState({
+        hide: false
+      })
+    }
+  }
+
   render() {
+    let navbar = this.state.hide ? "navbar navbar-expand-lg navbar-light fixed-top navbar-shrink" : "navbar navbar-expand-lg navbar-light fixed-top"
     return (
-      <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
-      <div class="container">
-        <a class="navbar-brand js-scroll-trigger" href="#page-top">Start Bootstrap</a>
-        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+      <nav className={navbar} id="mainNav" onScroll={this.navbarCollapse}>
+      <div className="container">
+        <a className="navbar-brand js-scroll-trigger" href="#page-top">Home</a>
+        <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
           Menu
-          <i class="fas fa-bars"></i>
+          <i className="fas fa-bars"></i>
         </button>
-        <div class="collapse navbar-collapse" id="navbarResponsive">
-          <ul class="navbar-nav ml-auto">
-            <li class="nav-item">
-              <a class="nav-link js-scroll-trigger" href="#about">About</a>
+        <div className="collapse navbar-collapse" id="navbarResponsive">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <a className="nav-link js-scroll-trigger" href="#about">About</a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link js-scroll-trigger" href="#projects">Projects</a>
+            <li className="nav-item">
+              <a className="nav-link js-scroll-trigger" href="#projects">Projects</a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link js-scroll-trigger" href="#signup">Contact</a>
+            <li className="nav-item">
+              <a className="nav-link js-scroll-trigger" href="#signup">Contact</a>
             </li>
           </ul>
         </div>
