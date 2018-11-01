93a2c52b1f135a65a35a37f231ecadf0ecb2f583
Steven <stevenhuynh17@gmail.com>
2018-11-01 11:56:59 -0700

    refactor: made svg files into react components

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -26,13 +26,21 @@ class Navigation extends Component {
   }
 
   render() {
-    let navbar = this.state.hide ? "navbar navbar-expand-lg navbar-light fixed-top navbar-shrink" : "navbar navbar-expand-lg navbar-light fixed-top"
+    let navbar = this.state.hide
+      ? "navbar navbar-expand-lg navbar-light fixed-top navbar-shrink"
+      : "navbar navbar-expand-lg navbar-light fixed-top"
     return (
       <nav className={navbar} id="mainNav" onScroll={this.navbarCollapse}>
       <div className="container">
         <a className="navbar-brand js-scroll-trigger" href="#page-top">Home</a>
-        <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-          Menu
+        <button
+          className="navbar-toggler navbar-toggler-right"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarResponsive"
+          aria-controls="navbarResponsive"
+          aria-expanded="false"
+          aria-label="Toggle navigation">
           <i className="fas fa-bars"></i>
         </button>
         <div className="collapse navbar-collapse" id="navbarResponsive">
