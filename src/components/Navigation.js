19ae8997b8ce9008b05c4a406700e3025159d30d
Steven <stevenhuynh17@gmail.com>
2018-12-07 15:26:57 -0800

    scroll to home should work

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -35,7 +35,7 @@ class Navigation extends Component {
     return (
       <nav className={navbar} id="mainNav" onScroll={this.navbarCollapse}>
       <div className="container">
-        <a className="navbar-brand" onClick={() => this.scroll(header)}>Home</a>
+        <a className="navbar-brand" onClick={(event) => scroll(header, about)}>Home</a>
         <button
           className="navbar-toggler navbar-toggler-right"
           type="button"
